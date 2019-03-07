f26160acd2c42ac5044cacb546507ae8cb113e0b
adriangrahl <adrian.grahl@gmail.com>
2019-03-07 01:23:39 -0300

    React Native App

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TodoList</Text>
+      </View>
+    );
+  }
+}
+
+// const mapStateToProps = state => ({});
+
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators(Actions, dispatch);
+
+export default TodoList;
